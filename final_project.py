2adb0f4095f71badb5707de173a46608aca63876
Eric Wasserman <wassy@dhcp-10-250-134-55.harvard.edu>
2019-04-17 19:17:30 -0400

    added main file

---
+++ final_project.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+import numpy as np
+import math
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
