9877e3b4507c1bfd136b163f1828a652461734ea
wfinigan <wfinigan64@gmail.com>
2019-05-07 14:28:18 -0400

    Data cleaning file

--- data_prep.py
+++ data_prep.py
@@ -22,8 +22,8 @@ df[df.Timestamp > start_time].to_csv('data/btc/price.csv')
 
 """# Get average fees per day for ETH.
 """
-df_fees_eth = pd.read_csv('data_historical/helper/eth/fees_raw.csv')
-df_counts_eth = pd.read_csv('data_historical/helper/eth/block_count.csv')
+df_fees_eth = pd.read_csv('data/helper/eth/fees_raw.csv')
+df_counts_eth = pd.read_csv('data/helper/eth/block_count.csv')
 
 # convert dates
 df_fees_eth['Date(UTC)'] = df_fees['Date(UTC)'].apply(lambda x: datetime.strptime(x, '%m/%d/%y'))
@@ -32,11 +32,11 @@ df_counts_eth['Date(UTC)'] = df_counts['Date(UTC)'].apply(lambda x: datetime.str
 df_fees_day_eth = pd.DataFrame()
 df_fees_day_eth['date'] = df_fees['Date(UTC)']
 df_fees_day_eth['avg_fee'] = (df_fees.Value / df_counts.Value) / 10**18
-df_fees_day_eth.to_csv('data_historical/eth/fees.csv')
+df_fees_day_eth.to_csv('data/eth/fees.csv')
 
 """Get bitcoin block count file from total supply change.
 """
-df_supply = pd.read_csv('data_historical/helper/btc/supply.csv', names=['date', 'supply'])
+df_supply = pd.read_csv('data/helper/btc/supply.csv', names=['date', 'supply'])
 
 df_supply['money_added'] = df_supply['supply'].diff()
 df_supply = clean_dates(df_supply)
@@ -45,24 +45,24 @@ df_counts_btc = pd.DataFrame()
 df_counts_btc['date'] = df_supply.date
 df_counts_btc['counts'] = df_supply.money_added / 12.5
 df_counts_btc.counts = df_counts_btc.counts.astype(int)
-df_counts_btc.to_csv('data_historical/helper/btc/block_count.csv', index=False)
+df_counts_btc.to_csv('data/helper/btc/block_count.csv', index=False)
 
 """# Clean up BTC fees file.
 """
-df_fees_btc = pd.read_csv('data_historical/helper/btc/fees_raw.csv', names=['date', 'avg_fees'])
+df_fees_btc = pd.read_csv('data/helper/btc/fees_raw.csv', names=['date', 'avg_fees'])
 df_fees_btc = clean_dates(df_fees_btc)
 
 df_fees_btc = df_fees_btc.reset_index(drop=True)
 
-df_counts_btc = pd.read_csv('data_historical/helper/btc/block_count.csv')
+df_counts_btc = pd.read_csv('data/helper/btc/block_count.csv')
 df_fees_btc.avg_fees = df_fees_btc.avg_fees / df_counts_btc.counts
 
-df_fees_btc.to_csv('data_historical/btc/fees.csv')
+df_fees_btc.to_csv('data/btc/fees.csv')
 
 
 """# Clean up BTC price file.
 """
-df_price_btc_raw = pd.read_csv('data_historical/helper/btc/price_raw.csv', index_col=0)
+df_price_btc_raw = pd.read_csv('data/helper/btc/price_raw.csv', index_col=0)
 df_price_btc = pd.DataFrame()
 
 df_price_btc['date'] = df_price_btc_raw.Timestamp
@@ -72,11 +72,11 @@ df_price_btc.head()
 df_price_btc.date = df_price_btc.date.astype(df_fees_btc.date.dtype)
 df_price_btc = df_price_btc[(df_price_btc.date >= start_time) & (df_price_btc.date < end_time)]
 df_price_btc = df_price_btc.reset_index(drop=True)
-df_price_btc.to_csv('data_historical/btc/price.csv')
+df_price_btc.to_csv('data/btc/price.csv')
 
 """# Clean up ETC price file.
 """
-df_price_eth_raw = pd.read_csv('data_historical/helper/eth/price_raw.csv')
+df_price_eth_raw = pd.read_csv('data/helper/eth/price_raw.csv')
 df_price_eth_raw.head()
 
 df_price_eth = pd.DataFrame()
@@ -85,4 +85,38 @@ df_price_eth = df_price_eth[(df_price_eth.date >= start_time) & (df_price_eth.da
 
 df_price_eth['price'] = df_price_eth_raw.Value
 df_price_eth = df_price_eth.reset_index(drop=True)
-df_price_eth.to_csv('data_historical/eth/price.csv')
+df_price_eth.to_csv('data/eth/price.csv')
+
+"""# Clean up OHLC data.
+"""
+df_ohlc_btc_raw = pd.read_csv('data/helper/btc/ohlc_raw.csv')
+df_ohlc_eth_raw = pd.read_csv('data/helper/eth/ohlc_raw.csv')
+
+df_ohlc_btc = pd.DataFrame()
+df_ohlc_eth = pd.DataFrame()
+
+
+df_ohlc_btc['date'] = df_ohlc_btc_raw.Date.astype(df_fees_btc.date.dtype)
+df_ohlc_eth['date'] = df_ohlc_eth_raw.Date.astype(df_fees_btc.date.dtype)
+
+df_ohlc_btc['adj_close'] = df_ohlc_btc_raw['Adj Close']
+df_ohlc_eth['adj_close'] = df_ohlc_eth_raw['Adj Close']
+
+df_ohlc_btc = df_ohlc_btc.reset_index(drop=True)
+df_ohlc_eth = df_ohlc_eth.reset_index(drop=True)
+
+# get smaller most recent date of both DF's
+most_recent = min(df_ohlc_btc.date[len(df_ohlc_btc) - 1], df_ohlc_eth.date[len(df_ohlc_eth) - 1])
+
+df_ohlc_btc = df_ohlc_btc[
+    (df_ohlc_btc.date <= most_recent) & (df_ohlc_btc.date > most_recent - relativedelta(years=1))
+]
+df_ohlc_eth = df_ohlc_eth[
+    (df_ohlc_eth.date <= most_recent) & (df_ohlc_eth.date > most_recent - relativedelta(years=1))
+]
+
+df_ohlc_btc = df_ohlc_btc.reset_index(drop=True)
+df_ohlc_eth = df_ohlc_eth.reset_index(drop=True)
+
+df_ohlc_btc.to_csv('data/btc/ohlc.csv')
+df_ohlc_eth.to_csv('data/eth/ohlc.csv')
