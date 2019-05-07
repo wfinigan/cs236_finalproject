33da5fc24992e4fea79236cee31d6a6b6c84a20f
wfinigan <wfinigan64@gmail.com>
2019-05-07 17:54:22 -0400

    Minor changes to data cleaning.

--- data_prep.py
+++ data_prep.py
@@ -9,31 +9,23 @@ def clean_dates(df):
 
     return df
 
-"""# Convert Bitcoin historical price data to timestamp format.
-"""
-df = pd.read_csv('coinbase_historical.csv')
-df.Timestamp = df.Timestamp.apply(lambda x: datetime.utcfromtimestamp(x))
-
 end_time = datetime(2019, 1,  1)
 start_time = end_time - relativedelta(years=1)
 
-df[df.Timestamp > start_time].to_csv('data/btc/price.csv')
-
-
 """# Get average fees per day for ETH.
 """
-df_fees_eth = pd.read_csv('data/helper/eth/fees_raw.csv')
+df_fees_eth = pd.read_csv('data/helper/eth/fees_total.csv')
 df_counts_eth = pd.read_csv('data/helper/eth/block_count.csv')
 
 # convert dates
-df_fees_eth['Date(UTC)'] = df_fees['Date(UTC)'].apply(lambda x: datetime.strptime(x, '%m/%d/%y'))
-df_counts_eth['Date(UTC)'] = df_counts['Date(UTC)'].apply(lambda x: datetime.strptime(x, '%m/%d/%y'))
+df_fees_eth['Date(UTC)'] = df_fees_eth['Date(UTC)'].apply(lambda x: datetime.strptime(x, '%m/%d/%y'))
+df_counts_eth['Date(UTC)'] = df_counts_eth['Date(UTC)'].apply(lambda x: datetime.strptime(x, '%m/%d/%y'))
 
 df_fees_day_eth = pd.DataFrame()
-df_fees_day_eth['date'] = df_fees['Date(UTC)']
-df_fees_day_eth['avg_fee'] = (df_fees.Value / df_counts.Value) / 10**18
-df_fees_day_eth.to_csv('data/eth/fees.csv')
+df_fees_day_eth['date'] = df_fees_eth['Date(UTC)']
+df_fees_day_eth['fees'] = (df_fees_eth.Value / df_counts_eth.Value) / 10**18
 
+df_fees_day_eth.to_csv('data/eth/fees.csv')
 """Get bitcoin block count file from total supply change.
 """
 df_supply = pd.read_csv('data/helper/btc/supply.csv', names=['date', 'supply'])
@@ -49,18 +41,18 @@ df_counts_btc.to_csv('data/helper/btc/block_count.csv', index=False)
 
 """# Clean up BTC fees file.
 """
-df_fees_btc = pd.read_csv('data/helper/btc/fees_raw.csv', names=['date', 'avg_fees'])
+df_fees_btc = pd.read_csv('data/helper/btc/fees_raw.csv', names=['date', 'fees'])
 df_fees_btc = clean_dates(df_fees_btc)
 
 df_fees_btc = df_fees_btc.reset_index(drop=True)
 
 df_counts_btc = pd.read_csv('data/helper/btc/block_count.csv')
-df_fees_btc.avg_fees = df_fees_btc.avg_fees / df_counts_btc.counts
+df_fees_btc.fees = df_fees_btc.fees / df_counts_btc.counts
 
 df_fees_btc.to_csv('data/btc/fees.csv')
 
 
-"""# Clean up BTC price file.
+"""Clean up BTC Price file.
 """
 df_price_btc_raw = pd.read_csv('data/helper/btc/price_raw.csv', index_col=0)
 df_price_btc = pd.DataFrame()
@@ -73,6 +65,7 @@ df_price_btc.date = df_price_btc.date.astype(df_fees_btc.date.dtype)
 df_price_btc = df_price_btc[(df_price_btc.date >= start_time) & (df_price_btc.date < end_time)]
 df_price_btc = df_price_btc.reset_index(drop=True)
 df_price_btc.to_csv('data/btc/price.csv')
+
 
 """# Clean up ETC price file.
 """
